6c0c213e62f030887054ed95066d8bdd7a13673e
Lucas Wiebbelling <lucaswie@outlook.com>
2020-02-27 17:40:40 -0300

    Special move - Castling

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -22,11 +22,11 @@ public abstract class ChessPiece extends Piece {
 		return moveCount;
 	}
 	
-	public void increaseAccount() {
+	public void increaseMoveCount() {
 		moveCount++;
 	}
 	
-	public void decreaseAccount() {
+	public void decreaseMoveCount() {
 		moveCount--;
 	}
 	
